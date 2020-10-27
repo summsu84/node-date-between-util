d32a1f662e4bff31db0165239eed7f3f323f93fd
summsu84 <summsu84@gmail.com>
2020-10-27 09:06:12 +0900

    get all date add

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 const betweenDateUtil = require('./index').BetweenDateUtil;
 
-console.log(betweenDateUtil.getBetweenDates('2020-10-01', '2020-10-10'));
+console.log(betweenDateUtil.getWorkingDateListBetweenDates('2020-10-01', '2020-10-10'));
+console.log(betweenDateUtil.getAllDateListBetweenDates('2020-10-01', '2020-10-10'));
