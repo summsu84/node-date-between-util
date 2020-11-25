8447820d9d8637c58dea3bff3d4bbd1a4958ffb0
summsu84 <summsu84@gmail.com>
2020-11-25 11:14:48 +0900

    es6 버전 추가 테스트

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 const betweenDateUtil = require('./index').BetweenDateUtil;
+//import betweenDateUtilEs6 from './lib/between-date-util-es6.js';
 
 console.log(betweenDateUtil.getWorkingDateListBetweenDates('2020-10-01', '2020-10-10'));
 console.log(betweenDateUtil.getAllDateListBetweenDates('2020-10-01', '2020-10-10'));
+
+//console.log(betweenDateUtilEs6.getWorkingDateListBetweenDates('2020-10-01', '2020-10-10'));
+//console.log(betweenDateUtilEs6.getAllDateListBetweenDates('2020-10-01', '2020-10-10'));
+
