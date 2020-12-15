df43c6e54304943eabb2fbad9ab95a92c2505eb0
summsu84 <summsu84@gmail.com>
2020-12-15 17:42:25 +0900

    버전 업그레이드

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ console.log(betweenDateUtil.getWorkingDateListBetweenDates('2020-10-01', '2020-1
 console.log(betweenDateUtil.getAllDateListBetweenDates('2020-10-01', '2020-10-10'));
 
 const sourceDateList = betweenDateUtil.getAllDateListBetweenDates('2020-10-01', '2020-10-11');
-const targetDateList = betweenDateUtil.getAllDateListBetweenDates('2020-10-11', '2020-10-20');
+const targetDateList = betweenDateUtil.getAllDateListBetweenDates('2020-10-12', '2020-10-20');
 
 const retVal = betweenDateUtil.checkContainTargetDateList(sourceDateList, targetDateList);
 console.log(`retVal : ${retVal}`);
